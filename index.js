03ed6fb4a4e84bd7a4eb9ee1853dee074507c88a
nabil <nabilblk@gmail.com>
2016-05-27 14:19:02 +0100

    Small change

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
 
+
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
   bot.reply(message, 'It\'s nice to talk to you directly.')
