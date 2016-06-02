99f6928ee66c2f6a3f38fa26bc0ed82827f6c0ab
nabil <nabilblk@gmail.com>
2016-06-02 20:46:49 +0100

    clean up for wit.ai

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Botkit = require('botkit')
 var WitBot = require('witbot')
-var SlideSearch = require('./slidesearch')
+//var SlideSearch = require('./slidesearch')
 
 var token = process.env.SLACK_TOKEN
 var witToken = process.env.WIT_TOKEN
+
+var witbot = WitBot(witToken)
 
 // WIT TOKEN 5TQ5OFRNLTHUK3CG6LFVJQ7SBJFC5QCT
 
@@ -40,6 +42,33 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+// wire up DMs and direct mentions to wit.ai
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+  var wit = witbot.process(message.text, bot, message)
+
+  wit.hears('hello', 0.53, function (bot, message, outcome) {
+    bot.startConversation(message, function (_, convo) {
+      convo.say('Hello!')
+      convo.ask('How are you?', function (response, convo) {
+        witbot.process(response.text)
+            .hears('good', 0.5, function (outcome) {
+              convo.say('I am so glad to hear it!')
+              convo.next()
+            })
+            .hears('bad', 0.5, function (outcome) {
+              convo.say('I\'m sorry, that is terrible')
+              convo.next()
+            })
+            .otherwise(function (outcome) {
+              convo.say('I\'m cofused')
+              convo.repeat()
+              convo.next()
+            })
+      })
+    })
+  })
+
+  wit.otherwise(function (bot, message) {
+    bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
+  })
 })
